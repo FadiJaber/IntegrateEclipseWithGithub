89c379c756902923513fd78fe75c73ec33b9b482
Fadi Asad <Fadi Asad@DESKTOP-GDH7SEE>
2018-01-23 17:46:52 +0200

    First Commit

---
+++ src/FadiJaber/Test.java
@@ -0,0 +1,10 @@
+package FadiJaber;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello All\n***Fadi Asad***");
+	}
+
+}
